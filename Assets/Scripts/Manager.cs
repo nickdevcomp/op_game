9fa96fceb90f945892e8b787a905bfe78fde6f9a
Konstantin Kolesnikov <kolesnikov.kons@gmail.com>
2024-05-29 23:17:38 +0500

    Update Menu

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -1,12 +1,47 @@
 using System;
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ButtonManager : MonoBehaviour
 {
-    public void Play() => SceneManager.LoadScene(1);
+    public Image fadeImage;
+    public AudioSource audioSource;
+    private readonly float fadeSpeed = 0.5f;
+    public AudioSource backgroundMusic;
+    private void Start()
+    {
+        fadeImage.GameObject().SetActive(false);
+        audioSource.Pause();
+    }
+
+    public void Play()
+    {
+        fadeImage.GameObject().SetActive(true);
+        backgroundMusic.Pause();
+        Invoke("Fade", 0);
+        Invoke("PlayAudio", 2f);
+        Invoke("LoadFirstScene", 9f);
+    }
+
+    private void Fade() =>  StartCoroutine(FadeIn());
+
+    private void PlayAudio() => audioSource.Play();
+
+    private void LoadFirstScene() => SceneManager.LoadScene(1);
+
+    private IEnumerator FadeIn()
+    {
+        var delta = 0.0f;
+        while (delta < 1)
+        {
+            delta += fadeSpeed * Time.deltaTime;
+            fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, delta);
+            yield return null;
+        }
+    }
 
     public void Exit() => Application.Quit();
 }
