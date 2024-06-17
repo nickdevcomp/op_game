c71e7cd86ab212c5dce58e2a5acce97273f8e018
Konstantin Kolesnikov <kolesnikov.kons@gmail.com>
2024-06-17 18:56:25 +0500

    Add start hint

---
+++ Assets/Scripts/StartHintFadeOut.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using UnityEngine;
+
+public class AnimationFadeOut : MonoBehaviour
+{
+    public float FadeDuration = 1.0f;
+    private bool hasKeyPressed;
+    private Coroutine fadeCoroutine;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (!hasKeyPressed && (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D)))
+        {
+            hasKeyPressed = true;
+            StartCoroutine(WaitAndStartFadeOut(1.3f)); 
+        }
+    }
+
+    private IEnumerator WaitAndStartFadeOut(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        fadeCoroutine = StartCoroutine(FadeOutAnimation());
+    }
+
+    private IEnumerator FadeOutAnimation()
+    {
+        float elapsedTime = 0.0f;
+        Color initialColor = spriteRenderer.color;
+
+        while (elapsedTime < FadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float newAlpha = Mathf.Lerp(initialColor.a, 0.0f, elapsedTime / FadeDuration);
+            spriteRenderer.color = new Color(initialColor.r, initialColor.g, initialColor.b, newAlpha);
+            yield return null;
+        }
+
+        spriteRenderer.color = new Color(initialColor.r, initialColor.g, initialColor.b, 0.0f);
+    }
+}
